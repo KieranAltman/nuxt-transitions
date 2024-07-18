78cb6ad8a74fb260bef07ead419821cf1e8e56ca
KieranAltman <zh_kai0531@163.com>
2024-07-18 19:50:10 +0800

    chore: clear useless code

--- src/module.ts
+++ src/module.ts
@@ -13,9 +13,6 @@ export default defineNuxtModule<ModuleOptions>({
   setup(options, nuxt) {
     const resolver = createResolver(import.meta.url)
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
-
     // components
     addComponentsDir({ path: resolver.resolve('./runtime/components') })
 
